54a4c1b58a3b2c3f848284d983479c93258142df
Jnsll <15047775+Jnsll@users.noreply.github.com>
2026-05-08 11:20:50 +0000

    Deploying to gh-pages from @ Jnsll/ResearchWebsite@1ecd06131136fd52ac071da029d0c8a17aac377e 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://jnsll.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://june.sallou.org/#contact">contact us</a>.'
               }
             ]
           }
