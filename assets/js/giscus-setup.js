f2823ad662f30b93c46849c6a6e7f6b00008b2eb
Jnsll <15047775+Jnsll@users.noreply.github.com>
2026-05-08 12:00:10 +0000

    Deploying to gh-pages from @ Jnsll/Jnsll.github.io@fb114df3aa2d57ebbd934694c9e4d39e62508f00 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Jnsll/ResearchWebsite",
+    "data-repo": "Jnsll/Jnsll.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
